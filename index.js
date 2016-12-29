4117fa10dea5038658393bfad858b866ea265fe6
Yarick <bjar@bk.ru>
2016-12-29 13:52:12 +0300

    Middleware

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 var passwordQuality = require('./dist/passwordQuality.min.js');
 var passwordQualityString = require('fs').readFileSync(__dirname + '/dist/passwordQuality.min.js', 'utf8');
 
-passwordQuality.toString = function() {
-    return passwordQualityString;
+passwordQuality.middleware = function (options) {
+    var url = require('url');
+
+    options = options || {};
+    options.path = options.path || '/password-quality';
+
+    return function (req, res, next) {
+        if (url.parse(req.url).pathname !== options.path) {
+            return next();
+        }
+
+        res.end(passwordQualityString)
+    }
 };
 
 module.exports = passwordQuality;
