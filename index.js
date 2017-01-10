4e312c33d0d2dee94637a9a16a9ea4cbf29fff3e
Vladimir Bulyga <xx@ccxx.cc>
2017-01-10 14:31:52 +0300

    change middleware

--- index.js
+++ index.js
@@ -1,19 +1,9 @@
-var passwordQuality = require('./dist/passwordQuality.min.js');
-var passwordQualityString = require('fs').readFileSync(__dirname + '/dist/passwordQuality.min.js', 'utf8');
+var dist = './dist/passwordQuality.min.js';
+var fs = require('fs');
+var path = require('path');
+var lib = fs.readFileSync(path.resolve(__dirname, dist), 'utf8');
 
-passwordQuality.middleware = function (options) {
-    var url = require('url');
-
-    options = options || {};
-    options.path = options.path || '/password-quality';
-
-    return function (req, res, next) {
-        if (url.parse(req.url).pathname !== options.path) {
-            return next();
-        }
-
-        res.end(passwordQualityString)
-    }
+module.exports = require(dist);
+module.exports.middleware = function (req, res) {
+    res.end(lib);
 };
-
-module.exports = passwordQuality;
